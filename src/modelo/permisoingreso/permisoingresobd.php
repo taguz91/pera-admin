5c488c49e4dbeb372b68d846d2523557b60aa517
Johnny <johnnygar98@hotmail.com>
2019-07-20 22:57:14 -0500

    Mejorando el codigo

--- src/modelo/permisoingreso/permisoingresobd.php
+++ src/modelo/permisoingreso/permisoingresobd.php
@@ -1,23 +1,13 @@
 <?php
-
 require_once("src/modelo/permisoingreso/permisoingreso.php");
 
 abstract class PermisoIngresoBD {
 
     static function guardar($permisoIngreso) {
-        $sql = '
-    INSERT INTO public."PermisoIngresoFichas"(
-    id_prd_lectivo, id_tipo_ficha, permiso_ingreso_fecha_inicio, permiso_ingreso_fecha_fin)
-    VALUES (:idPeriodo, :idTipoFicha, :fechaInicio, :fechaFin);
-    ';
-        echo $permisoIngreso->id;
-        echo $permisoIngreso->fechaInicio;
-        echo $permisoIngreso->fechaFin;
-
         $ct = getCon();
 
         if ($ct != null) {
-            $sentencia = $ct->prepare($sql);
+            $sentencia = $ct->prepare($this::INSERT);
             $res = $sentencia->execute([
                 'idPeriodo' => $permisoIngreso->idPeriodo,
                 'idTipoFicha' => $permisoIngreso->idTipoFicha,
@@ -36,43 +26,10 @@ abstract class PermisoIngresoBD {
         }
     }
 
-    static function eliminar($id) {
-        $sql = '
-    UPDATE public."PermisoIngresoFichas"
-    SET permiso_ingreso_activo=false
-    WHERE id_permiso_ingreso_ficha=:id;
-    ';
-
-        $ct = getCon();
-        if ($ct != null) {
-            $sentencia = $ct->prepare($sql);
-            $res = $sentencia->execute([
-                'id' => $id
-            ]);
-            if ($res != null) {
-                echo "<h1>Eliminamos correctamente</h1>";
-                return true;
-            }else{
-              return false;
-            }
-        }else{
-          return false;
-        }
-    }
-
     static function editar($permisoIngreso) {
-        $sql = '
-  UPDATE public."PermisoIngresoFichas"
-  SET id_prd_lectivo=:idPeriodo,
-  id_tipo_ficha=:idTipoFicha,
-  permiso_ingreso_fecha_inicio =: fechaInicio,
-  permiso_ingreso_fecha_fin =: fechaFin
-  WHERE id_permiso_ingreso_ficha=:id;
-  ';
-
         $ct = getCon();
         if ($ct != null) {
-            $sentencia = $ct->prepare($sql);
+            $sentencia = $ct->prepare($this::UPDATE);
             $res = $sentencia->execute([
                 'id' => $permisoIngreso->id,
                 'idPeriodo' => $permisoIngreso->idPeriodo,
@@ -87,6 +44,26 @@ abstract class PermisoIngresoBD {
               return false;
             }
         }else {
+          return false;
+        }
+    }
+
+
+    static function eliminar($id) {
+
+        $ct = getCon();
+        if ($ct != null) {
+            $sentencia = $ct->prepare($this::DELETE);
+            $res = $sentencia->execute([
+                'id' => $id
+            ]);
+            if ($res != null) {
+                echo "<h1>Eliminamos correctamente</h1>";
+                return true;
+            }else{
+              return false;
+            }
+        }else{
           return false;
         }
     }
@@ -111,17 +88,11 @@ abstract class PermisoIngresoBD {
           if ($res != null) {
               $pi = new PermisoIngresoMD();
               while($r = $res->fetch(PDO::FETCH_ASSOC)){
-
                 $pi->id = $r['id_permiso_ingreso_ficha'];
-
                 $pi->idPeriodo = $r['id_prd_lectivo'];
-
                 $pi->idTipoFicha = $r['id_tipo_ficha'];
-
                 $pi->fechaInicio = $r['permiso_ingreso_fecha_inicio'];
-
                 $pi->fechaFin = $r['permiso_ingreso_fecha_fin'];
-
 
               }
               return $pi;
@@ -132,46 +103,117 @@ abstract class PermisoIngresoBD {
     }
 
     static function getAll() {
-        $sql = '
-        SELECT
-        id_permiso_ingreso_ficha,
-        p.prd_lectivo_nombre,
-        tF.tipo_ficha,
-        pF.permiso_ingreso_fecha_inicio,
-        pF.permiso_ingreso_fecha_fin
-        FROM public."PermisoIngresoFichas" pF, public."PeriodoLectivo" p,public."TipoFicha" tF
-        WHERE pF.id_prd_lectivo=p.id_prd_lectivo
-        AND pF.id_tipo_ficha=tF.id_tipo_ficha
-        AND pF.permiso_ingreso_activo=true ;';
-
+        $sql = $this::BASEQUERY.' '.$this::ENDQUERY;
         $ct = getCon();
         if ($ct != null) {
             $res = $ct->query($sql);
             if ($res != null) {
-                $items = array();
-                while($r = $res->fetch(PDO::FETCH_ASSOC)){
-                  //var_dump($r);
-                  $pi = new PermisoIngresoMD();
-
-                  $pi->id = $r['id_permiso_ingreso_ficha'];
-
-                  $pi->idPeriodo = $r['prd_lectivo_nombre'];
-
-                  $pi->idTipoFicha = $r['tipo_ficha'];
-
-                  $pi->fechaInicio = $r['permiso_ingreso_fecha_inicio'];
-
-                  $pi->fechaFin = $r['permiso_ingreso_fecha_fin'];
-
-                  array_push($items, $pi);
-                }
-                return $items;
+                return $this::obtenerParaTbl($res);
             }else{
               return [];
             }
         }
     }
 
+    static function getPorPeriodo($idPeriodo){
+      $sql = $this::BASEQUERY
+      ." AND pl.id_prd_lectivo = $idPeriodo "
+      .$this::ENDQUERY;
+      $ct = getCon();
+      if ($ct != null) {
+          $res = $ct->query($sql);
+          if ($res != null) {
+              return $this::obtenerParaTbl($res);
+          }else{
+            return [];
+          }
+      }
+    }
+
+    static function getPorTipoFicha($idTipoFicha){
+      $sql = $this::BASEQUERY
+      ." AND tf.id_tipo_ficha = $idTipoFicha "
+      .$this::ENDQUERY;
+      $ct = getCon();
+      if ($ct != null) {
+          $res = $ct->query($sql);
+          if ($res != null) {
+              return $this::obtenerParaTbl($res);
+          }else{
+            return [];
+          }
+      }
+    }
+
+    static function buscar($aguja){
+      $sql = $this::BASEQUERY
+      ." AND pl.prd_lectivo_nombre ILIKE '%$aguja%' "
+      .$this::ENDQUERY;
+      $ct = getCon();
+      if ($ct != null) {
+          $res = $ct->query($sql);
+          if ($res != null) {
+              return $this::obtenerParaTbl($res);
+          }else{
+            return [];
+          }
+      }
+    }
+
+    private static function $obtenerParaTbl($res){
+      $items = array();
+      while($r = $res->fetch(PDO::FETCH_ASSOC)){
+        //var_dump($r);
+        $pi = new PermisoIngresoMD();
+        $pi->id = $r['id_permiso_ingreso_ficha'];
+        $pi->idPeriodo = $r['prd_lectivo_nombre'];
+        $pi->idTipoFicha = $r['tipo_ficha'];
+        $pi->fechaInicio = $r['permiso_ingreso_fecha_inicio'];
+        $pi->fechaFin = $r['permiso_ingreso_fecha_fin'];
+        array_push($items, $pi);
+      }
+      return $res;
+    }
+
+    private static $BASEQUERY = '
+    SELECT
+    id_permiso_ingreso_ficha,
+    p.prd_lectivo_nombre,
+    tf.tipo_ficha,
+    pf.permiso_ingreso_fecha_inicio,
+    pf.permiso_ingreso_fecha_fin
+    FROM
+    public."PermisoIngresoFichas" pf,
+    public."PeriodoLectivo" pl,
+    public."TipoFicha" tf
+    WHERE pf.id_prd_lectivo = pl.id_prd_lectivo
+    AND pf.id_tipo_ficha = tf.id_tipo_ficha
+    AND pf.permiso_ingreso_activo = true
+    ';
+
+    private static $ENDQUERY = '
+    ORDER BY
+    pf.permiso_ingreso_fecha_fin DESC,
+    pl.prd_lectivo_nombre;';
+
+    private static $INSERT = '
+    INSERT INTO public."PermisoIngresoFichas"(
+      id_prd_lectivo, id_tipo_ficha, permiso_ingreso_fecha_inicio, permiso_ingreso_fecha_fin)
+    VALUES(:idPeriodo, :idTipoFicha, :fechaInicio, :fechaFin)';
+
+    private static $UPDATE = '
+    UPDATE public."PermisoIngresoFichas"
+    SET id_prd_lectivo = :idPeriodo,
+    id_tipo_ficha = :idTipoFicha,
+    permiso_ingreso_fecha_inicio =: fechaInicio,
+    permiso_ingreso_fecha_fin =: fechaFin
+    WHERE id_permiso_ingreso_ficha =: id;';
+
+    private static $DELETE = '
+    UPDATE public."PermisoIngresoFichas"
+    SET permiso_ingreso_activo = false
+    WHERE id_permiso_ingreso_ficha=:id;
+    ';
 }
 
 ?>
