cac3e77f8de120cc1ce35c429ab3e9d931751315
Johnny <johnnygar98@hotmail.com>
2019-08-12 10:58:17 -0500

    Arreglado

--- src/controlador/seccionficha/seccionficha.php
+++ src/controlador/seccionficha/seccionficha.php
@@ -7,7 +7,7 @@ class SeccionFichaCTR extends CTR implements DCTR {
     function __construct()
     {
         parent::__construct("src/vista/seccionFichaV/");
-    
+
     }
 
     public function inicio(){
