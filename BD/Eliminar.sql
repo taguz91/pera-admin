e6f42884d969f85f2fb346edfae765fc90a29480
Johnny <johnnygar98@hotmail.com>
2019-09-03 17:58:46 -0500

    libreria para exportar a excel

---
+++ BD/Eliminar.sql
@@ -0,0 +1,19 @@
+DROP TABLE public."TipoFicha" CASCADE;
+
+DROP TABLE public."GrupoSocioeconomico" CASCADE;
+
+DROP TABLE public."SeccionesFicha" CASCADE;
+
+DROP TABLE public."PreguntasFicha" CASCADE;
+
+DROP TABLE public."RespuestaFicha" CASCADE;
+
+DROP TABLE public."PermisoIngresoFichas" CASCADE;
+
+DROP TABLE public."PersonaFicha" CASCADE;
+
+DROP TABLE publi."DocenteRespuestaFO" CASCADE;
+
+DROP TABLE public."AlumnoRespuestaFS" CASCADE;
+
+DROP TABLE public."AlumnoRespuestaLibreFS" CASCADE;
--- src/vista/templates/header.php
+++ src/vista/templates/header.php
@@ -2,10 +2,11 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Pera</title>
     <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/main.css">
-    <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/bootstrap.css">
+    <!--<link rel="stylesheet" href="<?php //echo constant('URL'); ?>public/css/bootstrap.css">-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script type="text/javascript" src="<?php echo constant('URL'); ?>public/js/main.js"></script>
   </head>
   <body>
 
